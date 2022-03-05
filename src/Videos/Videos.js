51dc0aec341f333000ef81d98fd0e4eb2a755e49
dishalparmar <dishal@hotmail.com>
2022-03-05 15:57:20 +0000

    Create Videos.js

---
+++ src/Videos/Videos.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Video from './Video';
+import './Videos.css';
+
+function Videos() {
+  return (
+    <div className='videos'>
+      <h3>Recommended</h3>
+      <div className='videos-recommended'>
+        <Video title='Tim Gets Reported To Cigars Daily HR'
+                views='6.6M' 
+                timestamp='3 days ago'
+                channel='Cigars Daily'
+                channelImage='https://yt3.ggpht.com/ytc/AKedOLRLwndAv1FiMnCf57Cyun0OCC8jzladn_xtPYw=s176-c-k-c0x00ffffff-no-rj'
+                image='https://i.ytimg.com/vi/Z7T14nbIFfc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBMQa_tizwYL-YcKl9PIZHaCJl-2w'
+        />
+        <Video title='Tim Gets Reported To Cigars Daily HR'
+                views='6.6M' 
+                timestamp='3 days ago'
+                channel='Cigars Daily'
+                channelImage='https://yt3.ggpht.com/ytc/AKedOLRLwndAv1FiMnCf57Cyun0OCC8jzladn_xtPYw=s176-c-k-c0x00ffffff-no-rj'
+                image='https://i.ytimg.com/vi/Z7T14nbIFfc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBMQa_tizwYL-YcKl9PIZHaCJl-2w'
+        />
+        <Video title='Tim Gets Reported To Cigars Daily HR'
+                views='6.6M' 
+                timestamp='3 days ago'
+                channel='Cigars Daily'
+                channelImage='https://yt3.ggpht.com/ytc/AKedOLRLwndAv1FiMnCf57Cyun0OCC8jzladn_xtPYw=s176-c-k-c0x00ffffff-no-rj'
+                image='https://i.ytimg.com/vi/Z7T14nbIFfc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBMQa_tizwYL-YcKl9PIZHaCJl-2w'
+        />
+        <Video title='Tim Gets Reported To Cigars Daily HR'
+                views='6.6M' 
+                timestamp='3 days ago'
+                channel='Cigars Daily'
+                channelImage='https://yt3.ggpht.com/ytc/AKedOLRLwndAv1FiMnCf57Cyun0OCC8jzladn_xtPYw=s176-c-k-c0x00ffffff-no-rj'
+                image='https://i.ytimg.com/vi/Z7T14nbIFfc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBMQa_tizwYL-YcKl9PIZHaCJl-2w'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Videos
